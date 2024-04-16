64e1452e27e02006aefffe006a355ff895cf1a8e
Dien-Nhung Nguyen-Phu <kein@kienlab.com>
2024-04-16 13:19:34 +0700

    dashboard: init

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chartRouter = require('./routes/chart');
 var apiRouter = require('./routes/api');
+var dashboardRouter = require('./routes/dashboard');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chart', chartRouter);
 app.use('/api', apiRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/dashboard.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	res.render('dashboard/dashboard', { 
+		title: 'Dashboard' ,
+
+		pwChConfig: {
+			baseUrl: 'http://rockwell.eproject.kienlab.com/api/raw',
+			dev_id: 'D4:8A:FC:A5:ED:E0',
+			devDataLen: 200,
+			predDataLen: 100,
+			dataLabel: 'Power (W)',
+			yAxisKey: 'P', // data field
+			xAxisKey: 'timestamp', // time field
+			chartTitle: "POWER",
+		}, 
+
+		consumChConfig: {
+			predBaseUrl: 'http://rockwell.eproject.kienlab.com/api/consum-fc',
+			devBaseUrl: 'http://rockwell.eproject.kienlab.com/api/consum',
+			dev_id: 'D4:8A:FC:A5:ED:E0',
+			chartTitle: "POWER CONSUMTION IN RECENT DATES",
+			dataLabel: 'Power Consumption (Wh)',
+			xAxisKey: 'consumption', // data field
+			yAxisKey: 'date', // time field
+		}
+	});
+});
+
+router.get('/consume', function(req, res, next) {
+	res.render('chart', { title: 'POWER CONSUMTION IN RECENT DATES' });
+});
+
+router.get('/power', function(req, res, next) {
+	res.render('chart', { title: 'Power' });
+});
+
+module.exports = router;
