de2a562b2d0bfe11aa078439d626af6bed753b9f
Dien-Nhung Nguyen-Phu <kein@kienlab.com>
2024-04-16 13:16:13 +0700

    chart: add stacked chart

--- routes/chart.js
+++ routes/chart.js
@@ -42,4 +42,19 @@ router.get('/power', function(req, res, next) {
 	});
 });
 
+router.get('/stacked', function(req, res, next) {
+	res.render('charts/stacked', {		
+		pwChConfig: {
+			baseUrl: 'http://rockwell.eproject.kienlab.com/api/raw',
+			dev_id: 'D4:8A:FC:A5:ED:E0',
+			devDataLen: 100,
+			predDataLen: 100,
+			dataLabel: 'Power (W)',
+			yAxisKey: 'P', // data field
+			xAxisKey: 'timestamp', // time field
+			chartTitle: "POWER",
+		}, 
+	});
+});
+
 module.exports = router;
