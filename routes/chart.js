f03e3154ca81e0595578d63b673bce6c338c4d4b
Dien-Nhung Nguyen-Phu <kein@kienlab.com>
2024-04-16 13:05:20 +0700

    chart: power: copy to route and views

--- routes/chart.js
+++ routes/chart.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.render('chart', { title: 'Chart' });
+	// res.render('charts/', { title: 'Dashboard' });
+	next();
+});
+
+router.get('/power', function(req, res, next) {
+	res.render('charts/power', {
+		title: 'POWER',
+		
+		pwChConfig: {
+			baseUrl: 'http://rockwell.eproject.kienlab.com/api/raw',
+			dev_id: 'D4:8A:FC:A5:ED:E0',
+			devDataLen: 100,
+			predDataLen: 100,
+			dataLabel: 'Power (W)',
+			yAxisKey: 'P', // data field
+			xAxisKey: 'timestamp', // time field
+			chartTitle: "POWER",
+		}, 
+	});
 });
 
 module.exports = router;
